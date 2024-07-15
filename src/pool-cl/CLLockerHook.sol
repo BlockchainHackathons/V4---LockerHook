e15a69062c8a672dcff197a0a24db8bb10fae3c1
pierrot498 <pierre.etienne.horreau@gmail.com>
2024-07-15 20:20:30 +0200

    add tes

--- src/pool-cl/CLLockerHook.sol
+++ src/pool-cl/CLLockerHook.sol
@@ -13,13 +13,14 @@ import {INonfungiblePositionManager} from "@pancakeswap/v4-periphery/src/pool-cl
 import {CLLockerData} from "./libraries/CLLockerData.sol";
 import {Constants} from "./libraries/Constants.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC721, ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 /// @notice CLCounterHook is a contract that counts the number of times a hook is called
 /// @dev note the code is not production ready, it is only to share how a hook looks like
-contract CLLockerHook is CLBaseHook {
+contract CLLockerHook is CLBaseHook, ERC721Enumerable {
     using PoolIdLibrary for PoolKey;
 
-    NonfungiblePositionManager nfp;
+    NonfungiblePositionManager public nfp;
 
     /// @notice The sender is not the hook
     error SenderMustBeHook();
@@ -32,6 +33,18 @@ contract CLLockerHook is CLBaseHook {
 
     /// @notice Not the holder of the nft
     error NotOwnerOfSelectedNFT();
+
+    /// @notice Insufficient liquidity in the position
+    error InsufficientLiquidity(uint128 available, uint128 required);
+
+    /// @notice Failed to transfer token
+    error TransferFailed();
+
+    /// @notice New unlock date must be in the future
+    error UnlockDateInFuture();
+
+    /// @notice New unlock date must be after the current unlock date
+    error UnlockDateAfterCurrent();
 
     event LiquidityAdded(
         PoolId poolId,
@@ -48,8 +61,11 @@ contract CLLockerHook is CLBaseHook {
 
     mapping(PoolId => mapping(uint256 => CLLockerData.LockInfo)) public lockInfo;
 
-    constructor(ICLPoolManager _poolManager) CLBaseHook(_poolManager) {
-        nfp = NonfungiblePositionManager(payable(0xe05b539447B17630Cb087473F6b50E5c5f73FDeb));
+    constructor(ICLPoolManager _poolManager, NonfungiblePositionManager _nfp) 
+        CLBaseHook(_poolManager) 
+        ERC721("Liquidity Position", "LQP")
+    {
+        nfp = _nfp;
     }
 
     function getHooksRegistrationBitmap() external pure override returns (uint16) {
@@ -81,8 +97,6 @@ contract CLLockerHook is CLBaseHook {
         ICLPoolManager.ModifyLiquidityParams calldata params,
         bytes calldata hookData
     ) external override poolManagerOnly returns (bytes4) {
-        // if (sender != address(this)) revert SenderMustBeHook();
-
         return this.beforeAddLiquidity.selector;
     }
 
@@ -93,7 +107,6 @@ contract CLLockerHook is CLBaseHook {
         ICLPoolManager.ModifyLiquidityParams calldata params,
         bytes calldata hookData
     ) external override poolManagerOnly returns (bytes4) {
-        // if (sender != address(this)) revert SenderMustBeHook();
         return this.beforeRemoveLiquidity.selector;
     }
 
@@ -108,20 +121,18 @@ contract CLLockerHook is CLBaseHook {
         });
         PoolId poolId = key.toId();
 
-        require(
-            IERC20(Currency.unwrap(params.currency0)).transferFrom(msg.sender, address(this), params.amount0Desired),
-            ""
-        );
-        require(
-            IERC20(Currency.unwrap(params.currency1)).transferFrom(msg.sender, address(this), params.amount1Desired),
-            ""
-        );
+        if (!IERC20(Currency.unwrap(params.currency0)).transferFrom(msg.sender, address(this), params.amount0Desired)) {
+            revert TransferFailed();
+        }
+        if (!IERC20(Currency.unwrap(params.currency1)).transferFrom(msg.sender, address(this), params.amount1Desired)) {
+            revert TransferFailed();
+        }
 
         IERC20(Currency.unwrap(params.currency0)).approve(address(nfp), params.amount0Desired);
         IERC20(Currency.unwrap(params.currency1)).approve(address(nfp), params.amount1Desired);
 
         (uint160 sqrtPriceX96, , , ) = poolManager.getSlot0(poolId);
-
+        
         if (sqrtPriceX96 == 0) revert PoolNotInitialized();
 
         INonfungiblePositionManager.MintParams memory mintParams = INonfungiblePositionManager.MintParams({
@@ -132,16 +143,18 @@ contract CLLockerHook is CLBaseHook {
             amount1Desired: params.amount1Desired,
             amount0Min: params.amount0Min,
             amount1Min: params.amount1Min,
-            recipient: params.to,
+            recipient: address(this),//params.to,
             deadline: params.deadline,
             salt: bytes32(0)
         });
-
+        
         (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1) = nfp.mint(mintParams);
 
         CLLockerData.LockInfo storage lock = lockInfo[poolId][tokenId];
 
         lock.unlockDate = params.unlockDate;
+
+        _mint(msg.sender, tokenId);
 
         emit LiquidityAdded(poolId, tokenId, liquidity, amount0, amount1, params.unlockDate);
     }
@@ -149,7 +162,9 @@ contract CLLockerHook is CLBaseHook {
     function decreaseLiquidity(
         CLLockerData.DecreaseLiquidityParams calldata params
     ) external returns (uint256 amount0, uint256 amount1) {
-        if (msg.sender == nfp.ownerOf(params.tokenId)) revert NotOwnerOfSelectedNFT();
+        if (ownerOf(params.tokenId) != msg.sender) {
+            revert NotOwnerOfSelectedNFT();
+        }
 
         PoolKey memory key = PoolKey({
             currency0: params.currency0,
@@ -163,7 +178,17 @@ contract CLLockerHook is CLBaseHook {
 
         CLLockerData.LockInfo memory lock = lockInfo[poolId][params.tokenId];
 
-        if (block.timestamp >= lock.unlockDate) revert PositionIsLocked();
+        if (block.timestamp <= lock.unlockDate) {
+            revert PositionIsLocked();
+        }
+
+        // Retrieve the position details
+        ( , , , , , , , uint128 liquidity, , , , , ) = nfp.positions(params.tokenId);
+
+        // Check if the position has enough liquidity to be decreased
+        if (liquidity < params.liquidity) {
+            revert InsufficientLiquidity(liquidity, params.liquidity);
+        }
 
         INonfungiblePositionManager.DecreaseLiquidityParams memory decreaseLiquidityParams = INonfungiblePositionManager
             .DecreaseLiquidityParams({
@@ -180,13 +205,19 @@ contract CLLockerHook is CLBaseHook {
     }
 
     function extendLock(uint256 tokenId, PoolId poolId, uint256 newUnlockDate) external {
-        if (msg.sender == nfp.ownerOf(tokenId)) revert NotOwnerOfSelectedNFT();
+        if (ownerOf(tokenId) != msg.sender) {
+            revert NotOwnerOfSelectedNFT();
+        }
 
-        require(newUnlockDate > block.timestamp, "New unlock date must be in the future");
+        if (newUnlockDate <= block.timestamp) {
+            revert UnlockDateInFuture();
+        }
 
         CLLockerData.LockInfo storage lock = lockInfo[poolId][tokenId];
 
-        require(newUnlockDate > lock.unlockDate, "New unlock date must be afterwards the current unlockDate");
+        if (newUnlockDate <= lock.unlockDate) {
+            revert UnlockDateAfterCurrent();
+        }
 
         lock.unlockDate = newUnlockDate;
 
--- test/pool-cl/CLLockerHook.t.sol
+++ test/pool-cl/CLLockerHook.t.sol
@@ -29,7 +29,7 @@ contract CLLockerHookTest is Test, CLTestUtils {
         // Use the existing vault, poolManager, and nfp from CLTestUtils
         (currency0, currency1) = deployContractsWithTokens();
 
-        hook = new CLLockerHook(poolManager);
+        hook = new CLLockerHook(poolManager, nfp);
 
         poolKey = PoolKey({
             currency0: currency0,
@@ -51,7 +51,7 @@ contract CLLockerHookTest is Test, CLTestUtils {
         IERC20(Currency.unwrap(currency0)).approve(address(nfp), type(uint256).max);
         IERC20(Currency.unwrap(currency1)).approve(address(nfp), type(uint256).max);
     }
-
+    
     function testPoolNotInitialized() public {
         // Ensure initial token balances are set
         MockERC20(Currency.unwrap(currency0)).mint(address(this), 1000000);
@@ -95,5 +95,111 @@ contract CLLockerHookTest is Test, CLTestUtils {
 
         vm.expectRevert(CLLockerHook.PoolNotInitialized.selector);
         hook.addLiquidity(params);
+    }
+
+    function testAddLiquidity(uint256 _unlock) public {
+        // Ensure initial token balances are set
+        MockERC20(Currency.unwrap(currency0)).mint(address(this), 1000000);
+        MockERC20(Currency.unwrap(currency1)).mint(address(this), 1000000);
+
+        uint256 amount0Desired = 1000;
+        uint256 amount1Desired = 1000;
+        uint256 amount0Min = 0;
+        uint256 amount1Min = 0;
+        uint256 deadline = block.timestamp + 1 hours;
+        int24 tickLower = -887220;
+        int24 tickUpper = 887220;
+        uint256 unlockDate = _unlock>0?_unlock:block.timestamp + 30 days;
+
+        CLLockerData.AddLiquidityParams memory params = CLLockerData.AddLiquidityParams({
+            currency0: currency0,
+            currency1: currency1,
+            fee: poolKey.fee,
+            parameters: poolKey.parameters,
+            amount0Desired: amount0Desired,
+            amount1Desired: amount1Desired,
+            amount0Min: amount0Min,
+            amount1Min: amount1Min,
+            to: msg.sender,
+            tickLower: tickLower,
+            tickUpper: tickUpper,
+            deadline: deadline,
+            unlockDate: unlockDate
+        });
+
+        // Check if pool is initialized
+        PoolId poolId = poolKey.toId();
+        (uint160 sqrtPriceX96, , , ) = poolManager.getSlot0(poolId);
+        console.log("sqrtPRICE",sqrtPriceX96);
+        require(sqrtPriceX96 != 0, "Pool not initialized");
+        assert(sqrtPriceX96 != 0);
+        hook.addLiquidity(params);
+
+        // Additional checks can be done here to verify the state changes
+    }
+
+    function testDecreaseLiquidity() public {
+        // Add liquidity first
+        testAddLiquidity(1);
+        vm.warp(10000);
+        // Decrease liquidity
+        uint256 tokenId = 1; 
+        uint128 liquidity = 1000; 
+        uint256 amount0Min = 0;
+        uint256 amount1Min = 0;
+
+        CLLockerData.DecreaseLiquidityParams memory params = CLLockerData.DecreaseLiquidityParams({
+            tokenId: tokenId,
+            liquidity: liquidity,
+            currency0: currency0,
+            currency1: currency1,
+            fee: poolKey.fee,
+            parameters: poolKey.parameters,
+            amount0Min: amount0Min,
+            amount1Min: amount1Min
+        });
+       
+        hook.decreaseLiquidity(params);
+    }
+
+       function testDecreaseLiquidityLocked() public {
+        // Add liquidity first
+        testAddLiquidity(block.timestamp+30 days);
+
+        // Decrease liquidity
+        uint256 tokenId = 1; 
+        uint128 liquidity = 100; 
+        uint256 amount0Min = 0;
+        uint256 amount1Min = 0;
+
+        CLLockerData.DecreaseLiquidityParams memory params = CLLockerData.DecreaseLiquidityParams({
+            tokenId: tokenId,
+            liquidity: liquidity,
+            currency0: currency0,
+            currency1: currency1,
+            fee: poolKey.fee,
+            parameters: poolKey.parameters,
+            amount0Min: amount0Min,
+            amount1Min: amount1Min
+        });
+
+        // ensure the token is owned by the contract for this test
+        vm.expectRevert(CLLockerHook.PositionIsLocked.selector);
+        hook.decreaseLiquidity(params);
+    }
+
+    function testExtendLock() public {
+        // Add liquidity first
+        testAddLiquidity(block.timestamp);
+
+        uint256 tokenId = 1; 
+        PoolId poolId = poolKey.toId();
+        uint256 newUnlockDate = block.timestamp + 60 days;
+
+        // ensure the token is owned by the contract for this test
+        hook.extendLock(tokenId, poolId, newUnlockDate);
+
+        //add assert
+        assert(hook.lockInfo(poolId,tokenId)==newUnlockDate);
     }
 }
